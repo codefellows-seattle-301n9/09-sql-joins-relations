34f0c93c5675075f4d1c237517bdd62522ed6d77
dknore <darcy.knore@gmail.com>
2017-12-30 17:53:57 -0800

    completed todo's

--- ronnie-darcy/server.js
+++ ronnie-darcy/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = 'postgress://localhost:5432/mvc_blog';
+const conString = 'postgress://localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', error => {
@@ -40,8 +40,8 @@ app.get('/articles', (request, response) => {
 
 app.post('/articles', (request, response) => {
   client.query(
-    '',
-    [],
+    'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
+    [request.body.author, request.body.authorUrl],
     function(err) {
       if (err) console.error(err);
       // REVIEW: This is our second query, to be executed when this first query is complete.
@@ -51,8 +51,9 @@ app.post('/articles', (request, response) => {
 
   function queryTwo() {
     client.query(
-      ``,
-      [],
+      `SELECT * FROM authors
+      WHERE author=$1`,
+      [request.body.author],
       function(err, result) {
         if (err) console.error(err);
 
@@ -64,8 +65,8 @@ app.post('/articles', (request, response) => {
 
   function queryThree(author_id) {
     client.query(
-      ``,
-      [],
+      `INSERT INTO articles(author_id, title, category, "publishedOn", body) VALUES ($1, $2, $3, $4, $5)`,
+      [author_id, request.body.title, request.body.category, request.body.publishedOn, request.body.body],
       function(err) {
         if (err) console.error(err);
         response.send('insert complete');
@@ -76,13 +77,19 @@ app.post('/articles', (request, response) => {
 
 app.put('/articles/:id', function(request, response) {
   client.query(
-    ``,
-    []
+    `UPDATE authors 
+     SET author=$1, "authorUrl"=$2
+     WHERE author_id=$3;
+    `,
+    [request.body.author, request.body.authorUrl, request.body.author_id],
   )
     .then(() => {
       client.query(
-        ``,
-        []
+        `UPDATE articles
+         SET author_id=$1, title=$2, category=$3, "publishedOn"=$4, body=$5
+         WHERE article_id=$6;
+        `,
+        [request.body.author_id, request.body.title, request.body.category, request.body.publishedOn, request.body.body, request.params.article_id]
       )
     })
     .then(() => {
